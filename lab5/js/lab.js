fbc46c79b5fdb0e9725dcb8f4df07a7c2869c492
naomiwalzer <102751717+naomiwalzer@users.noreply.github.com>
2022-04-19 17:02:19 -0700

    ok

---
+++ lab5/js/lab.js
@@ -0,0 +1,23 @@
+/**
+* Author:    Naomi Walzer <nwalzer@ucsc.edu>
+* Created:   4.19.2022
+*
+* (c) Copyright by Blub Corp.
+**/
+
+// define variables
+var make = "Toyota";
+var model = "Matrix";
+var color = "White";
+var year = 2010;
+var ownIt = true;
+
+// calculate
+var age = 2022 - year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age of car: " + age + "years<br>");
