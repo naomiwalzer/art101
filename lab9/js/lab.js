7a976533cf5740e9fb687ff702d2fc8bd641ea5c
naomiwalzer <102751717+naomiwalzer@users.noreply.github.com>
2022-05-10 15:25:13 -0700

    what happened

---
+++ lab9/js/lab.js
@@ -0,0 +1,24 @@
+/**
+* Author:    Naomi Walzer <nwalzer@ucsc.edu>
+* Created:   5.9.2022
+*
+* (c) Copyright by Blub Corp.
+**/
+
+
+var outputEl =
+  document.getElementById("output");
+
+var new1El =
+document.createElement("p");
+
+  outputEl.appendChild(new1El);
+
+  new1El.innerHTML = "This is very fun";
+
+
+var new2El = document.createElement("p");
+
+  output2El.appendChild(new2El);
+
+  new2El.innerHTML = "I can't believe it";
