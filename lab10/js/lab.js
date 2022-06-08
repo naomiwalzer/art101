0b1baeeede5d21de441098a36f332e2e414ac68f
naomiwalzer <102751717+naomiwalzer@users.noreply.github.com>
2022-06-07 18:15:28 -0700

    hey

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -5,26 +5,33 @@
   * (c) Copyright by Blub Corp.
   **/
 
-  //Step 2 of Task 2
-  function retrieveUserName(){
-       //Step 3 of Task 2
-       var userName = window.prompt("Give me your username!");
-       console.log("userName=",userName);
-       //Step 4 of Task 2
-       //changed to an array
-       var usernameArray = userName.split('');
-       console.log("usernameArray=", usernameArray);
-       //sorting the array
-       var usernameArraySort = usernameArray.sort();
-       console.log("usernameArraySort=", usernameArraySort);
-       //array turend back to string
-       var sortedUsername = usernameArraySort.join();
-       console.log("sortedUsername=", sortedUsername);
-       //Step 5 of Task 2
-       return sortedUsername;
+
+  console.log("JS has loaded!");
+
+  //primary function called by the HTML button.
+  function runScramble() {
+    let name = getUsername();
+    console.log(name);
+    scramble(name)
   }
+   //simple function to get username from user
+function getUsername() {
+  let name = prompt("Please enter your name:","Your Name");
+  return name;
+}
 
+//scramble the username
+function scramble(username) {
+  console.log(username);
 
-  //Step 6 of Task 2
-  document.writeln("Your name will now be given back alphabetized",
-       retrieveUserName(), "</br>");
+//split the string
+var nameArraySort = nameArray.sort(function(a, b){return 0.5 - Math.random()});
+console.log("nameSorted=", nameSorted);
+
+//join array back to a string
+var nameSorted = nameArray.join('');
+console.log("nameSorted=", nameSorted);
+
+//print new name: fills a div with a hard coded P element
+document.getElementById("output").innerHTML = "<p id='output'>Here is your nem name: <b>" + nameSorted + "</p>";
+}
